5da0e700ea5d7557232c00d67ad31a4b3002d52a
tahirshahazad5@gmail.com <tahirshahazad5@gmail.com>
2022-07-02 15:14:24 +0500

    quiz 2 commit

--- tahircollection/Controllers/HomeController.cs
+++ tahircollection/Controllers/HomeController.cs
@@ -19,6 +19,22 @@ namespace tahircollection.Controllers
         }
 
 
+        public IActionResult Index1(string fanme, string lname,string email,string phone,string gender,string department)
+        {
+            if(fanme =="text" && email == "email")
+            {
+                ViewBag.message = "data racived";
+
+            }
+            else
+            {
+                ViewBag.message = "data not racived";
+
+            }
+            return View("Index1");
+        }
+
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
